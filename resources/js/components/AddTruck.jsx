92e7fb9f41886976bdd9db3606c379c51bdce6a3
zabiRahimi <rahimi.z1360@gmail.com>
2024-02-22 16:45:50 +0330

    six4

--- resources/js/components/AddTruck.jsx
+++ resources/js/components/AddTruck.jsx
@@ -1,13 +1,15 @@
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 import Title from "./hooks/Title";
 import "../../css/formBeton.css";
 
-const AddTruck=()=>{
-    return(
+import iran from "../../assets/images/iran.png";
+
+const AddTruck = () => {
+    return (
         <>
-        <Title title='تعریف کامیون' />
-        <div>
+            <Title title="تعریف کامیون" />
+            <div>
                 <form action="" className="formBeton">
                     <div className="sectionFB">
                         <div className="divInputFB">
@@ -29,13 +31,14 @@ const AddTruck=()=>{
                             <select name="" id="" className="selectFB">
                                 <option value="">انتخاب</option>
                                 <option value="میکسر">میکسر</option>
-                                <option value="پمپ هوایی دکل">پمپ هوایی دکل</option>
+                                <option value="پمپ هوایی دکل">
+                                    پمپ هوایی دکل
+                                </option>
                                 <option value="پمپ زمینی">پمپ زمینی</option>
                                 <option value="کمپرسی">کمپرسی</option>
                                 <option value="تریلر بونکر">تریلر بونکر</option>
                                 <option value="لودر">لودر</option>
                                 <option value="جرثقیل">جرثقیل</option>
-
                             </select>
                         </div>
                     </div>
@@ -43,44 +46,117 @@ const AddTruck=()=>{
                     <div className="sectionFB">
                         <div className="divInputFB">
                             <label> پلاک </label>
-                            <input type="text" className="" />
-                            <select name="" id="">
-                                <option value=""> الف </option>
-                                <option value=""> ب </option>
-                                <option value=""> پ </option>
-                                <option value=""> ت </option>
-                                <option value=""> ث </option>
-                                <option value=""> ج </option>
-                                <option value=""> چ </option>
-                                <option value=""> ح </option>
-                                <option value=""> خ </option>
-                                <option value=""> د </option>
-                                <option value=""> ذ </option>
-                                <option value=""> ر </option>
-                                <option value=""> ز </option>
-                                <option value=""> ژ </option>
-                                <option value=""> س </option>
-                                <option value=""> ش </option>
-                                <option value=""> ص </option>
-                                <option value=""> ض </option>
-                                <option value=""> ط </option>
-                                <option value=""> ظ </option>
-                                <option value=""> ع </option>
-                                <option value=""> غ </option>
-                                <option value=""> ف </option>
-                                <option value=""> ق </option>
-                                <option value=""> ک </option>
-                                <option value=""> گ </option>
-                                <option value=""> ل </option>
-                                <option value=""> م </option>
-                                <option value=""> ن </option>
-                                <option value=""> و </option>
-                                <option value=""> ه </option>
-                                <option value=""> ی </option>
-                            </select>
-                        </div>
+                            <div className="divPlak">
+                                <div className="divExamplePlak">
+                                    <div className="divIranPlak">
+                                        <div className="iranPlakDivImg">
+                                            <img
+                                                className="imgNSP"
+                                                src={iran}
+                                            />
+                                        </div>
+                                        <div className="iranPlakDivSpans">
+                                            <span>I.R.</span>
+                                            <span>IRAN</span>
+                                        </div>
+                                    </div>
+                                    <div className="divNamberPlak">
+                                        <input
+                                            type="text"
+                                            name=""
+                                            id=""
+                                            className="text2numberPlaK"
+                                            placeholder="00"
+                                        />
+                                        <select name="" id="" className="selectChPlak">
+                                            <option value=""> الف </option>
+                                            <option value=""> ب </option>
+                                            <option value=""> پ </option>
+                                            <option value=""> ت </option>
+                                            <option value=""> ث </option>
+                                            <option value=""> ج </option>
+                                            <option value=""> چ </option>
+                                            <option value=""> ح </option>
+                                            <option value=""> خ </option>
+                                            <option value=""> د </option>
+                                            <option value=""> ذ </option>
+                                            <option value=""> ر </option>
+                                            <option value=""> ز </option>
+                                            <option value=""> ژ </option>
+                                            <option value=""> س </option>
+                                            <option value=""> ش </option>
+                                            <option value=""> ص </option>
+                                            <option value=""> ض </option>
+                                            <option value=""> ط </option>
+                                            <option value=""> ظ </option>
+                                            <option value=""> ع </option>
+                                            <option value=""> غ </option>
+                                            <option value=""> ف </option>
+                                            <option value=""> ق </option>
+                                            <option value=""> ک </option>
+                                            <option value=""> گ </option>
+                                            <option value=""> ل </option>
+                                            <option value=""> م </option>
+                                            <option value=""> ن </option>
+                                            <option value=""> و </option>
+                                            <option value=""> ه </option>
+                                            <option value=""> ی </option>
+                                        </select>
+                                        <input
+                                            type="text"
+                                            name=""
+                                            id=""
+                                            className="text3numberPlaK"
+                                            placeholder="000"
+                                        />
+                                    </div>
+                                    <div className="divSerialPlak">
+                                        <span>ایران</span>
+                                        <input
+                                            type="text"
+                                            className="textSerialPlaK"
+                                            placeholder="00"
+                                        />
+                                    </div>
+                                </div>
 
-                        
+                                {/* <input type="text" className="" />
+                                <select name="" id="">
+                                    <option value=""> الف </option>
+                                    <option value=""> ب </option>
+                                    <option value=""> پ </option>
+                                    <option value=""> ت </option>
+                                    <option value=""> ث </option>
+                                    <option value=""> ج </option>
+                                    <option value=""> چ </option>
+                                    <option value=""> ح </option>
+                                    <option value=""> خ </option>
+                                    <option value=""> د </option>
+                                    <option value=""> ذ </option>
+                                    <option value=""> ر </option>
+                                    <option value=""> ز </option>
+                                    <option value=""> ژ </option>
+                                    <option value=""> س </option>
+                                    <option value=""> ش </option>
+                                    <option value=""> ص </option>
+                                    <option value=""> ض </option>
+                                    <option value=""> ط </option>
+                                    <option value=""> ظ </option>
+                                    <option value=""> ع </option>
+                                    <option value=""> غ </option>
+                                    <option value=""> ف </option>
+                                    <option value=""> ق </option>
+                                    <option value=""> ک </option>
+                                    <option value=""> گ </option>
+                                    <option value=""> ل </option>
+                                    <option value=""> م </option>
+                                    <option value=""> ن </option>
+                                    <option value=""> و </option>
+                                    <option value=""> ه </option>
+                                    <option value=""> ی </option>
+                                </select> */}
+                            </div>
+                        </div>
                     </div>
 
                     <div className="sectionFB">
@@ -88,23 +164,24 @@ const AddTruck=()=>{
                             <label> مالک خودرو </label>
                             <input type="text" className="inputTextFB" />
                         </div>
-
-                        
                     </div>
 
-                    
-
-                   
                     <div className="sectionFB divBtnsFB">
                         {/* <input type="button" >ثبت</input> */}
-                        <Button variant="success" className="btnSaveFB"> ثبت </Button>
-                        <Button type="reset" variant="warning" > پاک کن </Button>
+                        <Button variant="success" className="btnSaveFB">
+                            {" "}
+                            ثبت{" "}
+                        </Button>
+                        <Button type="reset" variant="warning">
+                            {" "}
+                            پاک کن{" "}
+                        </Button>
                         {/* <input type="reset" value={'پاک کردن'} /> */}
                     </div>
                 </form>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default AddTruck;
