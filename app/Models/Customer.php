28d79d0fbf35de37026502c24c665e25336585ba
zabiRahimi <rahimi.z1360@gmail.com>
2024-03-15 20:19:28 +0330

    composer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+// use Morilog\Jalali\jDate;
 
 class Customer extends Model
 {
     use HasFactory;
+
+    // public function getCreatedAtAttribute($value)
+    // {
+    //     return jDate::forge($value)->format('%Y-%m-%d %H:%M:%S');
+    // }
+
+    // public function getUpdatedAtAttribute($value)
+    // {
+    //     return jDate::forge($value)->format('%Y-%m-%d %H:%M:%S');
+    // }
 }
--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Morilog\Jalali\JalaliServiceProvider::class,
+
     ])->toArray(),
 
     /*
@@ -183,6 +185,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        // 'jDate' => Morilog\Jalali\Facades\jDate::class,
+
     ])->toArray(),
 
 ];
