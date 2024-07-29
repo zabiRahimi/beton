5ac859bb440b1b3d0e8d9c09cbde8b2c006a9f10
zabiRahimi <rahimi.z1360@gmail.com>
2024-07-29 22:33:27 +0330

    8 mordad

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
@@ -17,7 +18,16 @@ class CustomerFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name'=> fake()->name(),
+            'lastName'=> fake()->lastName(),
+            'father'=> fake()->name(),
+            'nationalCode'=> Str::random(10),
+            'dateOfBirth'=>,
+            'mobile'=>,
+            'telephone'=>,
+            'email'=>fake()->unique()->safeEmail(),
+            'postalCode'=>,
+            'address'=>,
         ];
     }
 }
