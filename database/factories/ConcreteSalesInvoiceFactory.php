98ba3990f8eb6665d382e65721764315ca95067a
zabiRahimi <rahimi.z1360@gmail.com>
2024-08-30 11:28:09 +0330

    9 shahrivar ConcreteSalesInvoiceFactory and seeder

--- database/factories/ConcreteSalesInvoiceFactory.php
+++ database/factories/ConcreteSalesInvoiceFactory.php
@@ -13,13 +13,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ConcreteSalesInvoiceFactory extends Factory
 {
-    protected  $date = ['year'=>1400, 'month'=>10, 'day'=>25];
-     protected $repeatDate = 3;
+    protected  $date = ['year' => 1400, 'month' => 1, 'day' => 1];
+    protected $time = ['hour' => 7, 'minutes' => 00, 'seconds' => 00];
+    protected $repeatDate = 3;
     protected const CONCRETE_ID = [1, 2, 3, 4, 5, 6, 7, 8];
     protected const CEMENT_STORE_ID = [1, 2];
 
-    protected const TIME=['06-08-00', '06-15-00', '06-20-00', '06-30-00', '06-50-00', '07-01-00', '07-10-00', '07-16-00', '07-58-00', '08-12-00', '08-17-00', '08-34-00', '08-49-00', '09-01-00', '09-11-00', '09-21-00', '10-30-00', '10-51-00', '11-14-00', '11-26-00', '11-34-00', '11-42-00', '12-25-00', '12-38-00', '12-49-00', '13-21-00', '13-35-00', '13-42-00', '14-23-00', '14-36-00', '15-02-00', '15-47-00', '16-12-00', '16-29-00', '17-32-00', '17-46-00', '17-59-00', '18-21-00', '18-37-00', '19-12-00', '19-28-00', '19-41-00', '20-25-00', '20-37-00', '20-50-00'];
-    
+    protected const TIME = ['06-08-00', '06-15-00', '06-20-00', '06-30-00', '06-50-00', '07-01-00', '07-10-00', '07-16-00', '07-58-00', '08-12-00', '08-17-00', '08-34-00', '08-49-00', '09-01-00', '09-11-00', '09-21-00', '10-30-00', '10-51-00', '11-14-00', '11-26-00', '11-34-00', '11-42-00', '12-25-00', '12-38-00', '12-49-00', '13-21-00', '13-35-00', '13-42-00', '14-23-00', '14-36-00', '15-02-00', '15-47-00', '16-12-00', '16-29-00', '17-32-00', '17-46-00', '17-59-00', '18-21-00', '18-37-00', '19-12-00', '19-28-00', '19-41-00', '20-25-00', '20-37-00', '20-50-00'];
+
     public function definition(): array
     {
 
@@ -35,7 +36,7 @@ class ConcreteSalesInvoiceFactory extends Factory
         $concreteId = $this->faker->randomElement(self::CONCRETE_ID);
 
         $cementStoreId = $this->faker->randomElement(self::CEMENT_STORE_ID);
-        $time = $this->faker->randomElement(self::TIME);
+        // $time = $this->faker->randomElement(self::TIME);
 
         return [
             'customer_id' => $customerId,
@@ -44,7 +45,7 @@ class ConcreteSalesInvoiceFactory extends Factory
             'concrete_id' => $concreteId,
             'cementStore_id' => $cementStoreId,
             'date' => $this->date(),
-            'time' => $time,
+            'time' => $this->time(),
             'weight' => 16100,
             'cubicMeters' => 7,
             'unitPrice' => 1000000,
@@ -59,23 +60,45 @@ class ConcreteSalesInvoiceFactory extends Factory
 
     public function date()
     {
-        $resalt=Carbon::create($this->date['year'] , $this->date['month']  , $this->date['day'] );
-        --$this->repeatDate;
-        if ($this->repeatDate == 0) {
-            $this->date['day']++;
-            if($this->date['day']==30 && $this->date['month']==12){
-                $this->date['year']++;
-                $this->date['month']=1;
-                $this->date['day']=1;
-            }elseif($this->date['day']==30 ){
-                $this->date['month']++;
-                $this->date['day']=1;
-            }
-            $this->repeatDate=1;
+        $resalt = Carbon::create($this->date['year'], $this->date['month'], $this->date['day']);
+        $this->repeatDate--;
+        $this->time['minutes'] = $this->time['minutes'] + 10;
+        if ($this->time['minutes'] > 58) {
+            $this->time['hour']++;
+            // $this->time['minutes'] = $this->date['day'];
+            $this->time['minutes'] = $this->date['day'];
         }
-        $resalt= $resalt->format('Y-m-d');
+
+        
+
+        if ($this->repeatDate == 1) {
+            $this->date['day']++;
+            
+            // $this->time['minutes'] = $this->date['day'];
+            if ($this->date['day'] == 30 && $this->date['month'] == 12) {
+                $this->date['year']++;
+                $this->date['month'] = 1;
+                $this->date['day'] = 1;
+            } elseif ($this->date['day'] == 30) {
+                $this->date['month']++;
+                $this->date['day'] = 1;
+            }
+            $this->repeatDate = mt_rand(3,8);
+            
+            if ($this->time['hour']>=18) {
+                $this->time['hour'] =7;
+            }
+
+        }
+        $resalt = $resalt->format('Y-m-d');
         return $resalt;
     }
 
-   
+    public function time()
+    {
+
+
+        $resalt = sprintf('%02d-%02d-%02d', $this->time['hour'], $this->time['minutes'], $this->time['seconds']);
+        return $resalt;
+    }
 }
