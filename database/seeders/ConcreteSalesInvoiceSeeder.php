a5367bc2d57e2445f8d1d29981b091f520b6669c
zabiRahimi <rahimi.z1360@gmail.com>
2024-09-03 08:56:50 +0330

    update ConcreteSalesInvoiceSeeder

--- database/seeders/ConcreteSalesInvoiceSeeder.php
+++ database/seeders/ConcreteSalesInvoiceSeeder.php
@@ -13,6 +13,6 @@ class ConcreteSalesInvoiceSeeder extends Seeder
      */
     public function run(): void
     {
-        ConcreteSalesInvoice:: factory(10)->create();
+        ConcreteSalesInvoice:: factory(3000)->create();
     }
 }
