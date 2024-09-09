9f483176c5253edd66fd707d751182bcfc18e167
zabiRahimi <rahimi.z1360@gmail.com>
2024-09-09 16:23:26 +0330

    update6

--- app/Http/Requests/GetConcreteSalesInvoiceRequest.php
+++ app/Http/Requests/GetConcreteSalesInvoiceRequest.php
@@ -22,7 +22,42 @@ class GetConcreteSalesInvoiceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'startDate' => 'nullable|date',
+            'endDate' => 'nullable|date',
+            'concrete_id' => 'nullable|numeric',
+            'cusotmer_id' => 'nullable|numeric',
+            'customerName' => 'nullable|string',
+            'custoemrLastName' => 'nullable|string',
+            'truck_id' => 'nullable|numeric',
+            'numberplate' => 'nullable|date',
+            'owner_id' => 'nullable|numeric',
+            'ownerName' => 'nullable|string',
+            'ownerLastName' => 'nullable|string',
+            'driver_id' => 'nullable|numeric',
+            'driverName' => 'nullable|string',
+            'driverLateName' => 'nullable|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'startDate.date' => 'تاریخ شروع جستجو را صحیح وارد کنید، چنانچه نمی‌خواهید تاریخ شروع را وارد کنید آن را کاملا پاک کنید',
+            'endDate.date' => 'تاریخ پایان جستجو را صحیح وارد کنید، چنانچه نمی‌خواهید تاریخ پایان را وارد کنید آن را کاملا پاک کنید',
+            'concrete_id.numeric' => 'شناسه بتن را به عدد وارد کنید',
+            'concrete_id.numeric' => 'شناسه مشتری را به عدد وارد کنید',
+            'customerName.string' => '|',
+            'custoemrLastName.string' => '',
+            'truck_id.numeric' => '',
+            'numberplate' => '|',
+            'owner_id.numeric' => '|',
+            'ownerName.string' => '|',
+            'ownerLastName.string' => '|',
+            'driver_id.numeric' => '|',
+            'driverNam.string' => '|',
+
+            'name.string' => 'نام مشتری را صحیح وارد کنید',
+            'lastName.string' => 'نام خانوادگی مشتری را صحیح وارد کنید',
         ];
     }
 }
