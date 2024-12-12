d28240de582dffa73239875b6a8324cff0efd0ea
zabiRahimi <rahimi.z1360@gmail.com>
2024-12-12 13:43:00 +0330

    up

--- beton_serve.bat
+++ beton_serve.bat
@@ -1,25 +1,23 @@
 @echo off
 REM راه‌اندازی XAMPP
 echo Starting XAMPP...
-cd C:\xampp
-xampp_start.exe
+start cmd /c "cd C:\xampp && xampp_start.exe"
 
 REM توقف برای اطمینان از راه‌اندازی کامل سرویس‌ها
 timeout /t 10
 
-REM تغییر مسیر به دایرکتوری پروژه لاراول
-cd C:\xampp\htdocs\beton
+REM تغییر مسیر به دایرکتوری پروژه ری‌اکت و اجرای دستور yarn dev
+echo Starting React development server...
+start cmd /k "cd C:\xampp\htdocs\beton && yarn dev"
 
-yarn dev
+REM توقف برای اطمینان از راه‌اندازی کامل سرور توسعه ری‌اکت
+timeout /t 10
 
-REM راه‌اندازی سرور لاراول
+REM تغییر مسیر به دایرکتوری پروژه لاراول و راه‌اندازی سرور لاراول
 echo Starting Laravel server...
+start cmd /k "cd C:\xampp\htdocs\beton && php artisan serve"
 
 REM باز کردن مرورگر فایرفاکس و اجرای برنامه
 start firefox http://localhost:8000
 
-@REM php artisan serve
-php -S localhost:8000 -t public
-
 pause
-
