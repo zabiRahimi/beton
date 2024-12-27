1ab93a99083f6367660e7e562d20e0a93ee1bcff
zabiRahimi <rahimi.z1360@gmail.com>
2024-12-27 13:48:26 +0330

    up

--- resources/js/components/invoices/sand/show/Show.jsx
+++ resources/js/components/invoices/sand/show/Show.jsx
@@ -1,7 +1,133 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import HeadPage from '../HeadPage';
+// import sandInvoicesearch from './sandInvoicesearch';
+import Skeleton from 'react-loading-skeleton';
+import Pagination from '../../../hooks/Pagination';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 const Show = () => {
+    const MySwal = withReactContent(Swal);
     const [loading, setLoading] = useState(false);
+    const hasCalledgetSandInvoices = useRef(false);
+    const [sandInvoices, setsandInvoices] = useState(null);
+    const [totalPage, setTotalPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalRecords, setTotalRecords] = useState(0);
+    const [search, setSearch] = useState({
+        startDate: '',//تاریخ ثبت حواله
+        endDate: '',
+        date: '',//تاریخ خرید حواله
+        id: '',
+        buyerName: '',
+        buyerLastName: '',
+        buyerFather: '',
+        remittanceNumber: '',
+        price: '',
+        isCompleted: true,
+        factory: ''
+    });
+
+    useEffect(() => {
+        if (!hasCalledgetSandInvoices.current) {
+            getSandInvoices();
+            hasCalledgetSandInvoices.current = true;
+        }
+    }, []);
+
+    async function getSandInvoices(page = 1, startDate = search.startDate,
+        endDate = search.endDate,
+        date = search.date,
+        id = search.id,
+        buyerName = search.buyerName,
+        buyerLastName = search.buyerLastName,
+        buyerFather = search.buyerFather,
+        remittanceNumber = search.remittanceNumber,
+        price = search.price,
+        isCompleted = search.isCompleted,
+        factory = search.factory) {
+
+        try {
+            setLoading(true);
+            const response = await axios.get(`/api/v1/sandInvoices`, {
+                params: { page, startDate, endDate, date, id, buyerName, buyerLastName, buyerFather, remittanceNumber, price, isCompleted, factory }
+            });
+
+            const datas = response.data.sandInvoices;
+            setsandInvoices(datas.data);
+            setTotalPage(datas.last_page);
+            setTotalRecords(datas.total);
+            window.scrollTo({ top: top, behavior: 'smooth' });
+        } catch (error) {
+            if (error.response && error.response.status === 422) {
+                const objErrors = error.response.data.errors;
+                const firstKey = Object.keys(objErrors)[0];
+                const firstValue = objErrors[firstKey][0];
+
+                MySwal.fire({
+                    icon: "warning",
+                    title: "هشدار",
+                    html: `<div style="color: red;">${firstValue}</div>`,
+                    confirmButtonText: "متوجه شدم!",
+                    confirmButtonColor: "#d33",
+                });
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    /**
+    * از طریق کامپوننت فرزند این متد فراخوانی و مقدار دهی می‌شود
+    * from AddCustomerSearch.jsx
+    * @param {} data 
+    */
+    const handelSetDataSearch = (data) => {
+        setSearch(data);
+    }
+
+    /**
+     * رکوردهای مشتریان ایجاد شده را با فرمت‌دهی مناسب جهت نمایش بر می گرداند
+     * @returns 
+     */
+    const returnSandInvoices = () => {
+        let length = sandInvoices.length;
+        if (length == 0) {
+            return <div className="notResultSearch_Se"> هیچ نتیجه‌ای یافت نشد!! </div>
+        }
+        let value = sandInvoices.map((sandInvoice, i) => {
+            let price = Number(sandInvoice['price']).toLocaleString();
+            let remainingPrice = Number(sandInvoice['remainingPrice']).toLocaleString();
+            let date = sandInvoice['date'].split('-');
+            let isCompleted = sandInvoice['isCompleted'] ? 'مانده' : 'تمام';
+            return <div className="rowListShowACSI_Ge" key={i}>
+                <span className="rowNumShowACSI_Ge">{i + 1}</span>
+                <span className="ticketNumberACSI_Ge">{sandInvoice['id']}</span>
+                <span className="remittanceNumber_Ge">{sandInvoice['remittanceNumber']}</span>
+                <span className="customerACSI_Ge buyerName_Ge">{sandInvoice['buyerName']}{'  '}{sandInvoice['buyerLastName']} {'  '} <span className='bueryFather_Ge'>{sandInvoice['buyerFather']}</span> </span>
+                <span className="price_Ge textAlignCenter_Ge">{price}</span>
+                <span className="price_Ge textAlignCenter_Ge">{remainingPrice}</span>
+                <span className="factory_Ge">{sandInvoice['factory']}</span>
+                <span className="dateACSI_Ge">{`${date[0]}/${date[1]}/${date[2]}`}</span>
+                <span className={`isCompleted_Ge ${sandInvoice['isCompleted'] ? 'true_Ge' : 'false_Ge'}`}>{isCompleted}</span>
+                <div className="divEditACSI_Ge">
+                    <Link className="--styleLessLink  btnEditACSI_Ge"
+                        title=" ویرایش "
+                        to={`/invoices/sandRemittance/edit/${sandInvoice['id']}`}
+                    >
+                        <i className="icofont-pencil iEditGe" />
+                    </Link>
+                </div>
+                <div className="divDelACSI_Ge">
+                    <button className="--styleLessBtn btnDelACSI_Ge" title=" حذف ">
+                        <i className="icofont-trash iDelGe" />
+                    </button>
+                </div>
+            </div>
+        })
+        return value;
+    }
+
     return (
         <div className=''>
             <HeadPage
@@ -10,6 +136,36 @@ const Show = () => {
                 displayBtnAdd={true}
                 displayBtnShow={false}
             />
+             <div className='containerShowGe containerShowCustomer' >
+                <div className="divListShowGe">
+                    {/* <sandInvoicesearch
+                        getSandInvoices={getSandInvoices}
+                        handelSetDataSearch={handelSetDataSearch}
+                        totalRecords={totalRecords}
+                    /> */}
+
+                    <div className="rowListShowGe headRowListShowGe rowListShowACSI_Ge">
+                        <span className="rowNumShowACSI_Ge ">ردیف</span>
+                        <span className="ticketNumberACSI_Ge ">شناسه</span>
+                        <span className="remittanceNumber_Ge ">شماره قبض</span>
+                        <span className="customerHeadACSI_Ge buyerName_Ge textAlignCenter_Ge">حواله</span>
+                        <span className="dumpTruckOwner_Ge textAlignCenter_Ge"> کمپرسی </span>
+                        <span className="price_Ge textAlignCenter_Ge"> شن‌‌‌وماسه </span>
+                        <span className="dateACSI_Ge textAlignCenter_Ge">تاریخ </span>
+                        <span className="editHeadShowACSI_Ge"> ویرایش  </span>
+                        <span className="delHeadShowACSI_Ge"> حذف </span>
+                    </div>
+                    {sandInvoices ? returnSandInvoices() : <Skeleton height={40} count={12} />}
+                    {/* <Pagination
+                        className="pagination-bar"
+                        currentPage={currentPage}
+                        totalPage={totalPage}
+                        siblingCount={3}
+                        onPageChange={page => { setCurrentPage(page); getSandInvoices(page) }}
+                    /> */}
+
+                </div>
+            </div>
         </div>
     )
 }
