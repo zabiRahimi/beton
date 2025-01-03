7580efcb0646d59bee3c7e65641bb608801cad50
zabiRahimi <rahimi.z1360@gmail.com>
2025-01-03 11:51:07 +0330

    up

---
+++ app/Http/Controllers/ProformaInvoiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProformaInvoice;
+use App\Http\Requests\StoreProformaInvoiceRequest;
+use App\Http\Requests\UpdateProformaInvoiceRequest;
+
+class ProformaInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProformaInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProformaInvoice $proformaInvoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProformaInvoice $proformaInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProformaInvoiceRequest $request, ProformaInvoice $proformaInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProformaInvoice $proformaInvoice)
+    {
+        //
+    }
+}
