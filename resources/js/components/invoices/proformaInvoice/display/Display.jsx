bbcacc49e9f6ece7303cccba2906bfd0dbaf2e9f
zabiRahimi <rahimi.z1360@gmail.com>
2025-01-08 13:50:30 +0330

    up

--- resources/js/components/invoices/proformaInvoice/display/Display.jsx
+++ resources/js/components/invoices/proformaInvoice/display/Display.jsx
@@ -3,6 +3,9 @@ import { useParams, Link } from 'react-router-dom';
 import HeadPage from '../HeadPage';
 import Skeleton from 'react-loading-skeleton';
 import Swal from 'sweetalert2';
+import '../../../../../css/proforma.css';
+
+import Standard from '../../../../../assets/images/standard.png'
 import withReactContent from 'sweetalert2-react-content';
 const Show = () => {
     const { proformaInvoiceId } = useParams();
@@ -11,14 +14,6 @@ const Show = () => {
     const [loading, setLoading] = useState(false);
     const hasCalledgetProforamInvoices = useRef(false);
     const [proformaInvoices, setProformaInvoices] = useState(null);
-
-  
-
-   
-
-   
-
-  
 
     return (
         <div className=''>
@@ -29,7 +24,21 @@ const Show = () => {
                 displayBtnShow={true}
             />
             <div className='containerShowGe containerShowCustomer' >
-              
+              <div className="mainProForma_PFo">
+                    <div className="containerProForma_PFo">
+                        <section className="hade1_PFo">
+                            <div className="standard_PFo">
+                                <img src={Standard} className='logoStandard_PFo' alt="علامت استاندارد ایران" />
+                            </div>
+                            <div className="title_PFo"></div>
+                            <div className="logoDateId_PFo"></div>
+                        </section>
+                        <section className="hade2_PFo"></section>
+                        <section className="body_PFo"></section>
+                        <section className="description_PFo"></section>
+                        <section className="footer_PFo"></section>
+                    </div>
+              </div>
             </div>
         </div>
     )
